37e4b8584147d1fc3557f5789e5b595ce15736a8
JCWashington <jasmine.washington412@gmail.com>
2022-04-01 19:47:03 -0400

    javascript files for all functionality

---
+++ public/js/signup.js
@@ -0,0 +1,28 @@
+const signupFormHandler = async function(event) {
+  event.preventDefault();
+
+  const usernameEl = document.querySelector('#username-input-signup');
+  const passwordEl = document.querySelector('#password-input-signup');
+
+  const response = await fetch('/api/user', {
+    method: 'POST',
+    body: JSON.stringify({
+      username: usernameEl.value,
+      password: passwordEl.value,
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  console.log(usernameEl.value);
+  console.log(passwordEl.value);
+
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert('Failed to sign up');
+  }
+};
+
+document
+  .querySelector('#signup-form')
+  .addEventListener('submit', signupFormHandler);
