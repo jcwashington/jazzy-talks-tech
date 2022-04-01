1879ab998a61269b6c7c1b2cd6e73de99c79732e
JCWashington <jasmine.washington412@gmail.com>
2022-04-01 09:13:32 -0400

    changed post_content to body to help with confusion

--- models/Post.js
+++ models/Post.js
@@ -5,31 +5,13 @@ class Post extends Model {};
 
 Post.init(
     {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
         name: {
             type: DataTypes.STRING,
             allowNull: false,
             unique: true,
         },
-        post_content: {
+        body: {
             type: DataTypes.STRING
-        },
-        date_created: {
-            type: DataTypes.DATE,
-            allowNull: false,
-            defaultValue: DataTypes.NOW,
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'user',
-                key: 'id',
-            },
         },
     },
     {
@@ -37,7 +19,7 @@ Post.init(
         timestamps: false,
         freezeTableName: true,
         underscored: true,
-        modelName: 'post',
+        modelName: 'Post',
     }
 );
 
